10f1504bbcc8f6bcba8eeb3012f479180764a51b
skariria <skariria@amazon.fr>
2025-01-13 20:18:34 +0100

    add of tgw in global hub account

--- global-hub/terraform/transit-gateway.tf
+++ global-hub/terraform/transit-gateway.tf
@@ -4,8 +4,7 @@ provider "aws" {
   region = "eu-west-3" # Replace with your desired region
 
   assume_role {
-    role_arn     = "arn:aws:iam::043309348131:role/aft-custo-account-role"
-    session_name = "TerraformAssumeRoleSession"
+    role_arn    = "{{ target_admin_role_arn }}"
   }
 }
 
