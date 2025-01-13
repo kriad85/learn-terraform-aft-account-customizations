1f2d31d534408af38bc373a6941ada2b5e0f63c0
skariria <skariria@amazon.fr>
2025-01-13 19:29:11 +0100

    add of tgw in global hub account

---
+++ global-hub/terraform/transit-gateway.tf
@@ -0,0 +1,32 @@
+# Provider configuration for the AFT management account using assume_role
+provider "aws" {
+  alias  = "controller"
+  region = "us-east-1" # Replace with your desired region
+
+  assume_role {
+    role_arn     = "arn:aws:iam::043309348131:role/aft-custo-account-role"
+    session_name = "TerraformAssumeRoleSession"
+  }
+}
+
+# Transit Gateway Resource in the Network Account
+resource "aws_ec2_transit_gateway" "example" {
+  description = "Transit Gateway for Networking"
+  tags = {
+    Name        = "Network-Transit-Gateway"
+    Environment = "Production"
+  }
+}
+
+# SSM Parameter in the AFT Management Account
+resource "aws_ssm_parameter" "transit_gateway_id" {
+  provider = aws.controller
+
+  name        = "/network/transit-gateway/id"
+  description = "Transit Gateway ID from the network account"
+  type        = "String"
+  value       = aws_ec2_transit_gateway.example.id
+  tags = {
+    Environment = "Dev"
+  }
+}
