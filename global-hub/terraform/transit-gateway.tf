84dbe2a9f45f29e35083caae3bc08b3386fe224a
skariria <skariria@amazon.fr>
2025-01-13 21:18:20 +0100

    add of tgw in global hub account

--- global-hub/terraform/transit-gateway.tf
+++ global-hub/terraform/transit-gateway.tf
@@ -17,6 +17,29 @@ resource "aws_ec2_transit_gateway" "example" {
   }
 }
 
+# Resource Access Manager (RAM) Resource Share
+resource "aws_ram_resource_share" "tgw_share" {
+  name = "TGW-Share"
+  allow_external_principals = false # Set to true if sharing with external organizations
+
+  tags = {
+    Name        = "TransitGatewayResourceShare"
+    Environment = "Production"
+  }
+}
+
+# Add the Transit Gateway to the Resource Share
+resource "aws_ram_resource_association" "tgw_association" {
+  resource_share_arn = aws_ram_resource_share.tgw_share.arn
+  resource_arn       = aws_ec2_transit_gateway.example.arn
+}
+
+# Add the Target Account as a Principal
+resource "aws_ram_principal_association" "tgw_principal" {
+  resource_share_arn = aws_ram_resource_share.tgw_share.arn
+  principal          = "050752649770" # Replace with the AWS Account ID of the target account
+}
+
 # SSM Parameter in the AFT Management Account
 resource "aws_ssm_parameter" "transit_gateway_id" {
   provider = aws.controller
